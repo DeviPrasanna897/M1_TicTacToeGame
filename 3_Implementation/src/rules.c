28a127611d71e29376297218c70f9cc5581d91e0
prasanna <75977407+DeviPrasanna897@users.noreply.github.com>
2022-04-03 16:32:02 +0530

    Add files via upload

---
+++ 3_Implementation/src/rules.c
@@ -0,0 +1,17 @@
+void rules()
+{
+    char link;
+    printf("\tTic-Tac-Toe\n\n");
+    printf("Welcome to the most played 2D game and a sort of fun using X and O\n\n");
+    printf("Rules:-\n");
+    printf("\n1:Each player will be entering the number to put respective X or O in the desired position");
+    printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or \n  vertically will be declared as the winner");
+    printf("\n\nEnjoy the game! Be a Winner!\n\n");
+    printf("For more clarifications press Y else type any other character:- ");
+    scanf("%c",&link);
+    if(link=='y' || link=='Y')
+    {
+        system("start http://www.wikihow.com/Play-Tic-Tac-Toe");
+    }
+
+}
